18286c7e1c962090c5484d14f60ce790398825e4
DavideFarci <yank92.df@gmail.com>
2025-04-28 00:41:17 +0200

    auth, user

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,25 @@
 from rest_framework import generics
 from django.contrib.auth.models import User
 from authentication.serializers import RegisterSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+
+class UserProfileView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        data = {
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "username": user.username,
+            "email": user.email,
+        }
+        return Response(data, status=status.HTTP_200_OK)
